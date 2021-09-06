e8f75797b3b237c4e17d9cc0baedbecbcbc497e8
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2021-09-06 08:44:59 +0100

    Create twosum.py

---
+++ twosum.py
@@ -0,0 +1,13 @@
+#Two sum
+class Solution:
+    def twoSum(self,nums,target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                sum = nums[i]+nums[j]
+                if sum==target:
+                    return[i,j]
